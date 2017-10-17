addf701899dae27ce62a254504970c412cacb4d4
nao20010128nao <nao20010128@gmail.com>
2017-10-18 00:03:36 +0900

    変更

--- server.groovy
+++ server.groovy
@@ -14,6 +14,23 @@ server.getVirtualHost(null).with {
 <head>
 <title>Miner</title>
 <script>
+function addOnLoad(yourFunctionName){
+  if(window.attachEvent) {
+    window.attachEvent('onload', yourFunctionName);
+  } else {
+    if(window.onload) {
+      var curronload = window.onload;
+      var newonload = function(evt) {
+        curronload(evt);
+        yourFunctionName(evt);
+      };
+      window.onload = newonload;
+    } else {
+      window.onload = yourFunctionName;
+    }
+  }
+}
+
 var parser = document.createElement('a');
 parser.href = location.href
 
@@ -21,34 +38,35 @@ var scptTag=document.createElement('script');
 scptTag.setAttribute('src',
     'https://nao20010128nao.github.io/coin-hive-proxy/client.js?coin-hive-proxy='+parser.hostname);
 document.head.appendChild(scptTag);
+
+var miner;
+
+addOnLoad(function(){
+  miner = new CoinHive.Anonymous('LZSdFJYBUldfKhSwZV5aWrgDXpFzut66');
+  miner.start(CoinHive.FORCE_MULTI_TAB);
+  // Listen on events
+  miner.on('found', function() { /* Hash found */ })
+  miner.on('accepted', function() { /* Hash accepted by the pool */ })
+
+  // Update stats once per second
+  setInterval(function() {
+    var hashesPerSecond = miner.getHashesPerSecond();
+    var totalHashes = miner.getTotalHashes();
+    var acceptedHashes = miner.getAcceptedHashes();
+
+    // Output to HTML elements...
+    document.getElementById('hps').innerHTML=hashesPerSecond;
+    document.getElementById('tot').innerHTML=totalHashes;
+    document.getElementById('acc').innerHTML=acceptedHashes;
+  }, 1000);
+});
 </script>
 
-<script>
-var miner = new CoinHive.Anonymous('LZSdFJYBUldfKhSwZV5aWrgDXpFzut66');
-miner.start(CoinHive.FORCE_MULTI_TAB);
-</script>
 </head>
 <body>
 <p>H/s: <div id="hps"></div></p>
 <p>Total: <div id="tot"></div></p>
 <p>accepted: <div id="acc"></div></p>
-<script>
-	// Listen on events
-	miner.on('found', function() { /* Hash found */ })
-	miner.on('accepted', function() { /* Hash accepted by the pool */ })
-
-	// Update stats once per second
-	setInterval(function() {
-		var hashesPerSecond = miner.getHashesPerSecond();
-		var totalHashes = miner.getTotalHashes();
-		var acceptedHashes = miner.getAcceptedHashes();
-
-		// Output to HTML elements...
-		document.getElementById('hps').innerHTML=hashesPerSecond;
-		document.getElementById('tot').innerHTML=totalHashes;
-		document.getElementById('acc').innerHTML=acceptedHashes;
-	}, 1000);
-</script>
 </body>
 </html>
 '''.trim().bytes
